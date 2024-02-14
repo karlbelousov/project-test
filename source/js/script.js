bf4445d2a0662a39047caada405067b23109ecc1
Белоусов Карл <141405429+karlbelousov@users.noreply.github.com>
2024-02-14 13:48:08 +0600

    модальные окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,40 @@
-/* в этот файл добавляет скрипты*/
+const cookies = document.querySelector('.cookies');
+const buttonsCookies = cookies.querySelectorAll('button');
+const modalOpenButtons = document.querySelectorAll('.button');
+const modalAuth = document.querySelector('.modal-auth');
+const modalAuthCancel = modalAuth.querySelector('.modal-auth__cancel');
+const success = document.querySelector('.success');
+const successButtons = success.querySelectorAll('button');
+
+function onCloseCookies() {
+  cookies.classList.add('hidden');
+}
+
+function openModalAuth(evt) {
+  if (evt.target.closest('.cookies__button')) {
+    return;
+  }
+  modalAuth.classList.remove('hidden');
+}
+
+function closeModalAuth() {
+  modalAuth.classList.add('hidden');
+}
+
+function closeSuccess() {
+  success.classList.add('hidden');
+}
+
+buttonsCookies.forEach((button) => {
+  button.addEventListener('click', onCloseCookies);
+});
+
+modalOpenButtons.forEach((button) => {
+  button.addEventListener('click', openModalAuth);
+});
+
+modalAuthCancel.addEventListener('click', closeModalAuth);
+
+successButtons.forEach((button) => {
+  button.addEventListener('click', closeSuccess);
+});
