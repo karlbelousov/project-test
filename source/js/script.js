e618de7c26a6f89a7f21e38f381905dbe355fe33
Белоусов Карл <141405429+karlbelousov@users.noreply.github.com>
2024-02-14 19:23:01 +0600

    sticky header, состояния UI элементов

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,29 @@
 const cookies = document.querySelector('.cookies');
 const buttonsCookies = cookies.querySelectorAll('button');
-const modalOpenButtons = document.querySelectorAll('.button');
+const modalOpenButtons = document.querySelectorAll('#modal-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const modalAuthCancel = modalAuth.querySelector('.modal-auth__cancel');
 const success = document.querySelector('.success');
 const successButtons = success.querySelectorAll('button');
+const inputPhone = modalAuth.querySelector('#phone-number');
+const buttonSubmit = modalAuth.querySelector('.modal-auth__button');
+const headerNav = document.querySelector('.header__nav');
+
+function onHeaderSticky() {
+  if (window.pageYOffset > 40) {
+    headerNav.classList.add('sticky');
+  } else {
+    headerNav.classList.remove('sticky');
+  }
+}
+
+window.addEventListener('scroll', onHeaderSticky);
 
 function onCloseCookies() {
   cookies.classList.add('hidden');
 }
 
-function openModalAuth(evt) {
-  if (evt.target.closest('.cookies__button')) {
-    return;
-  }
+function openModalAuth() {
   modalAuth.classList.remove('hidden');
 }
 
@@ -38,3 +48,17 @@ modalAuthCancel.addEventListener('click', closeModalAuth);
 successButtons.forEach((button) => {
   button.addEventListener('click', closeSuccess);
 });
+
+const phoneMask = new IMask(inputPhone, {
+  mask: '+{7} (000) 000-00-00',
+});
+
+function onInputPhone() {
+  if (phoneMask.masked.isComplete) {
+    buttonSubmit.disabled = true;
+  } else {
+    buttonSubmit.disabled = false;
+  }
+}
+
+inputPhone.addEventListener('oninput', onInputPhone);
