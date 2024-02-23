66f62511fce056d64b3c4dbeaf3d4ae08325f13b
Белоусов Карл <141405429+karlbelousov@users.noreply.github.com>
2024-02-23 16:52:50 +0600

    правки вёрстки и валидации формы

--- source/js/modal-auth.js
+++ source/js/modal-auth.js
@@ -4,31 +4,19 @@ const modalOpenButtons = document.querySelectorAll('.open-modal-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const modalAuthCancel = modalAuth.querySelector('.modal-auth__cancel');
 const modalAuthForm = modalAuth.querySelector('.modal-auth__form');
-const nameInput = modalAuthForm.querySelector('#name');
-const nameEmail = modalAuthForm.querySelector('#email');
 const phoneInput = modalAuthForm.querySelector('#phone-number');
-const formSubmitButton = modalAuthForm.querySelector('.modal-auth__button');
 
 const phoneMask = new IMask(phoneInput, {
   mask: '+{7}(000)000-00-00',
 });
 
-const pristine = new Pristine(modalAuthForm, {
-  classTo: 'modal-auth__group',
-  errorClass: 'modal-auth__group--invalid',
-  errorTextParent: 'modal-auth__group',
-  errorTextClass: 'modal-auth__error-text',
-});
-
 function openModalAuth() {
   modalAuth.classList.remove('hidden');
-  formSubmitButton.disabled = true;
 }
 
 function closeModalAuth() {
   modalAuth.classList.add('hidden');
   modalAuthForm.reset();
-  pristine.reset();
 }
 
 function onKeyDownUp(evt) {
@@ -38,14 +26,6 @@ function onKeyDownUp(evt) {
   }
 }
 
-function onInput() {
-  const isValid = pristine.validate();
-  if (isValid && phoneMask.masked.isComplete) {
-    formSubmitButton.disabled = false;
-  } else {
-    formSubmitButton.disabled = true;
-  }
-}
 
 function onSubmitForm(evt) {
   evt.preventDefault();
@@ -61,6 +41,3 @@ modalOpenButtons.forEach((button) => {
 modalAuthCancel.addEventListener('click', closeModalAuth);
 modalAuthForm.addEventListener('submit', onSubmitForm);
 document.addEventListener('keydown', onKeyDownUp);
-nameEmail.addEventListener('input', onInput);
-nameInput.addEventListener('input', onInput);
-phoneInput.addEventListener('input', onInput);
